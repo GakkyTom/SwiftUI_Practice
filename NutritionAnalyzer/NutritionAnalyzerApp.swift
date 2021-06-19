bad6cd870cbe5472bc66a3ebb9032be40f4359ec
GakkyTom <itagakitomoya@itagakitomoyanoMacBook-Pro.local>
2021-06-19 23:33:41 +0900

    Chapter 1

---
+++ NutritionAnalyzer/NutritionAnalyzerApp.swift
@@ -0,0 +1,17 @@
+//
+//  NutritionAnalyzerApp.swift
+//  NutritionAnalyzer
+//
+//  Created by 板垣智也 on 2021/06/19.
+//
+
+import SwiftUI
+
+@main
+struct NutritionAnalyzerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
